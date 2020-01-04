c3bad22440324499eeed7184ecefb053f412d04a
tiptop-crazy <andrei.shewko@gmail.com>
2020-01-04 03:53:47 +0300

    two-lines triangle problems are solved

---
+++ triangle_quest.py
@@ -0,0 +1,29 @@
+"""
+Problem Statement
+You are given a positive integer N. You have to print a numerical triangle of height N−1 as shown below:
+1
+22
+333
+4444
+55555
+......
+Can you do it using only arithmetic operations, a single for loop and print statement?
+You can't take more than two lines; the first line (for - statement) is already written for you. You have to complete the print statement.
+Note Using anything related to strings will give a score of 0.
+Input Format 
+Given N in the first line.
+Constraints 
+1≤N≤9
+Output Format 
+Print N−1 lines as explained above.
+Sample Input
+5
+Sample Output
+1
+22
+333
+4444
+"""
+
+for i in range(1,input()): 
+    print ((pow(10,i)-1)/9)*i
